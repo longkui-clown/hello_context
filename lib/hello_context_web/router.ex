75403d0c9953bac818d6ce132baef9072e614624
long <315202350@qq.com>
2019-03-07 16:37:14 +0800

    增加登陆凭证限时失效

--- lib/hello_context_web/router.ex
+++ lib/hello_context_web/router.ex
@@ -7,6 +7,7 @@ defmodule HelloContextWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :ensure_session_not_timeout
   end
 
   pipeline :api do
@@ -33,6 +34,28 @@ defmodule HelloContextWeb.Router do
     pipe_through [:browser, Plug.Auth]
 
     resources "/pages", PageController
+  end
+
+  defp ensure_session_not_timeout(conn, _) do
+    now = Utils.timestamp()
+    timeout_s = Application.get_env(:hello_context, :timeout_s, 3600)
+    case {get_session(conn, :timeout), get_session(conn, :user_id)} do
+      {expire_timestamp, _} when expire_timestamp > now ->
+        conn
+        |> put_session(:timeout, now + timeout_s)
+
+      {_, nil} ->
+        conn
+        |> put_session(:timeout, now + timeout_s)
+      
+      _ ->
+        conn
+        |> clear_session()
+        |> Phoenix.Controller.put_flash(:error, "Long time no operation, Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      
+    end
   end
 
   # Other scopes may use custom stacks.
